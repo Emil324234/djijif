c4d99c8db1ebbc7c9080ef9a2223f60795bad9be
Emil324234 <145430552+Emil324234@users.noreply.github.com>
2023-10-04 12:31:43 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+class Program
+{
+    static void Tabl()
+    {
+        var tabl = new int[10, 10]; 
+        for (int i = 0; i < 10; ++ i)
+        {
+            for (int n = 1; n < 10; ++ n)
+            {
+                tabl[i, n] = i * n;
+                Console.Write(tabl[i, n] + "/t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
